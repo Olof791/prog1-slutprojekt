054616aaa2709becc37771d43d60ddd5fe8cbf77
Olof2008 <olof.bergsten@elev.ga.ntig.se>
2026-05-08 09:39:21 +0200

    gjort lite saker

--- träningslog.py
+++ träningslog.py
@@ -1,3 +1,5 @@
+import json
+
 pass_val= {
     "Armpass": ["Preachercurl", "Sidedeltraises","Tricepexstension", "Inclinecurl"],
      "Pushpass": ["Hoist Inclinepress", "Pec deck", "Sidedeltraises", "Tricepexstension"],
@@ -12,22 +14,26 @@ if pass_idag in pass_val:
     dagens_logg=[]
 
     print(f"{pass_idag}")
+         datum = input("Vad var datumet för passet? ")
 
     for öving in övingar:
         print(f"Övning: {öving}")
         sets = input("Hur många set gjorde du? ")
         reps = input("Hur många reps gjorde du? ")
         vikt = input("Vilken vikt använde du? ")
-        datum = input("Vad var datumet för passet?")
+        form = input("Var formen bra? (ja/nej) ")
 
         dagens_logg.append({
             "övning": öving,
             "sets": sets,
             "reps": reps,
             "vikt": vikt,
-            "datum": datum
+            "datum": datum,
+            "form": form
         })
 
     print("\nDagens logg:")
     for logg in dagens_logg:
-        print(f"Övning: {logg['övning']}, Sets: {logg['sets']}, Reps: {logg['reps']}, Vikt: {logg['vikt']} kg, Datum: {logg['datum']}")
+        print(f"Övning: {logg['övning']}, Sets: {logg['sets']}, Reps: {logg['reps']}, Vikt: {logg['vikt']} kg, Datum: {logg['datum']}, Form: {logg['form']}")
+
+    print(json.dumps(dagens_logg, indent=4))
